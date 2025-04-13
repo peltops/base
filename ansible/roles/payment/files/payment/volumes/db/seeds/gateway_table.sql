1f3c06f2663b9dab3c3c0fdae8127fd995bfde0d
nafas-rizkyfaturriza <rizky.faturriza@nafas.co.id>
2025-04-13 17:25:44 +0700

    feat: refactor gateway seeding to prevent duplicates in payment_gateway table

--- ansible/roles/payment/files/payment/volumes/db/seeds/gateway_table.sql
+++ ansible/roles/payment/files/payment/volumes/db/seeds/gateway_table.sql
@@ -1,4 +1,12 @@
 INSERT INTO "public.payment_gateway" ("gateway_id", "name", "created_at", "updated_at")
-VALUES
-    (uuid_generate_v4(), 'stripe', now(), now()),
-    (uuid_generate_v4(), 'midtrans', now(), now()),
+SELECT uuid_generate_v4(), gateway_name, now(), now()
+FROM (
+    VALUES 
+        ('stripe'),
+        ('midtrans')
+) AS t(gateway_name)
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM "public.payment_gateway" 
+    WHERE "name" = t.gateway_name
+);
