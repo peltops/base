1e65d2c1a9f0df111ba3095f53783f5e80c61b32
nafas-rizkyfaturriza <rizky.faturriza@nafas.co.id>
2025-04-14 11:38:44 +0700

    fix: correct table creation syntax and update foreign key references in SQL migration

--- ansible/roles/payment/files/payment/volumes/db/migrations/20250413153258_init.sql
+++ ansible/roles/payment/files/payment/volumes/db/migrations/20250413153258_init.sql
@@ -1,72 +1,72 @@
 -- migrate:up
-CREATE TABLE IF NOT EXISTS "public"."orders" (
-    "order_id" UUID NOT NULL UNIQUE,
-    "user_id" VARCHAR(255),
-    "total_amount" NUMERIC,
-    "currency" VARCHAR(255),
-    "status" VARCHAR(255),
-    "created_at" TIMESTAMPTZ,
-    "updated_at" TIMESTAMPTZ,
-    PRIMARY KEY("order_id")
-);
-
-CREATE TABLE IF NOT EXISTS "public"."order_items" (
-    "order_item_id" UUID NOT NULL UNIQUE,
+CREATE TABLE CREATE TABLE "public"."orders" (
 	"order_id" UUID NOT NULL UNIQUE,
-    "product_id" UUID NOT NULL UNIQUE,
-    "quantity" INTEGER,
-    "price" NUMERIC,
-    "created_at" TIMESTAMPTZ,
-    "updated_at" TIMESTAMPTZ,
-    PRIMARY KEY("order_item_id")
+	"user_id" VARCHAR(255),
+	"total_amount" NUMERIC,
+	"currency" VARCHAR(255),
+	"status" VARCHAR(255),
+	"created_at" TIMESTAMPTZ,
+	"updated_at" TIMESTAMPTZ,
+	PRIMARY KEY("order_id")
 );
 
-CREATE TABLE IF NOT EXISTS "public"."products" (
-    "product_id" UUID NOT NULL UNIQUE,
-    "name" VARCHAR(255),
-    "description" VARCHAR(255),
-    "price" NUMERIC,
-    "created_at" TIMESTAMPTZ,
-    "updated_at" TIMESTAMPTZ,
-    PRIMARY KEY("product_id")
+CREATE TABLE CREATE TABLE "public"."order_items" (
+	"order_item_id" UUID NOT NULL UNIQUE,
+	"order_id" UUID,
+	"product_id" UUID,
+	"quantity" INTEGER,
+	"price" NUMERIC,
+	"created_at" TIMESTAMPTZ,
+	"updated_at" TIMESTAMPTZ,
+	PRIMARY KEY("order_item_id")
 );
 
-CREATE TABLE IF NOT EXISTS "public"."payments" (
-    "payment_id" UUID NOT NULL UNIQUE,
-    "gateway_payment_id" VARCHAR(255),
-    "order_id" UUID NOT NULL UNIQUE,
-    "gateway_id" UUID NOT NULL UNIQUE,
-    "amount" NUMERIC,
-    "currency" VARCHAR(255),
-    "status" VARCHAR(255),
-    "created_at" TIMESTAMPTZ,
-    "updated_at" TIMESTAMPTZ,
-    "user_id" VARCHAR(255),
-    PRIMARY KEY("payment_id")
+CREATE TABLE CREATE TABLE "public"."products" (
+	"product_id" UUID NOT NULL UNIQUE,
+	"name" VARCHAR(255),
+	"description" VARCHAR(255),
+	"price" NUMERIC,
+	"created_at" TIMESTAMPTZ,
+	"updated_at" TIMESTAMPTZ,
+	PRIMARY KEY("product_id")
 );
 
-CREATE TABLE IF NOT EXISTS "public"."payment_gateway" (
-    "gateway_id" UUID NOT NULL UNIQUE,
-    "name" VARCHAR(255),
-    "created_at" TIMESTAMPTZ,
-    "updated_at" TIMESTAMPTZ,
-    PRIMARY KEY("gateway_id")
+CREATE TABLE CREATE TABLE "public"."payments" (
+	"payment_id" UUID NOT NULL UNIQUE,
+	"gateway_payment_id" VARCHAR(255),
+	"order_id" UUID,
+	"gateway_id" UUID,
+	"amount" NUMERIC,
+	"currency" VARCHAR(255),
+	"status" VARCHAR(255),
+	"created_at" TIMESTAMPTZ,
+	"updated_at" TIMESTAMPTZ,
+	"user_id" VARCHAR(255),
+	PRIMARY KEY("payment_id")
 );
 
-CREATE TABLE IF NOT EXISTS "public"."transactions" (
-    "transaction_id" UUID NOT NULL UNIQUE,
-    "payment_id" UUID NOT NULL UNIQUE,
-    "gateway_transaction_id" VARCHAR(255),
-    "gateway_response" TEXT,
-    "status" VARCHAR(255),
-    "created_at" TIMESTAMPTZ,
-    "updated_at" TIMESTAMPTZ,
-    PRIMARY KEY("transaction_id")
+CREATE TABLE CREATE TABLE "public"."payment_gateway" (
+	"gateway_id" UUID NOT NULL UNIQUE,
+	"name" VARCHAR(255),
+	"created_at" TIMESTAMPTZ,
+	"updated_at" TIMESTAMPTZ,
+	PRIMARY KEY("gateway_id")
 );
 
-ALTER TABLE "public"."orders"
-ADD FOREIGN KEY("order_id") REFERENCES "order_items"("order_id")
-ON UPDATE NO ACTION ON DELETE NO ACTION;
+CREATE TABLE CREATE TABLE "public"."transactions" (
+	"transaction_id" UUID NOT NULL UNIQUE,
+	"payment_id" UUID,
+	"gateway_transaction_id" VARCHAR(255),
+	"gateway_response" TEXT,
+	"status" VARCHAR(255),
+	"created_at" TIMESTAMPTZ,
+	"updated_at" TIMESTAMPTZ,
+	PRIMARY KEY("transaction_id")
+);
+
+ALTER TABLE "public"."order_items"
+ADD FOREIGN KEY("order_id") REFERENCES "orders"("order_id")
+ON UPDATE CASCADE ON DELETE CASCADE;
 
 ALTER TABLE "public"."order_items"
 ADD FOREIGN KEY("product_id") REFERENCES "products"("product_id")
@@ -76,12 +76,12 @@ ALTER TABLE "public"."payments"
 ADD FOREIGN KEY("order_id") REFERENCES "orders"("order_id")
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-ALTER TABLE "public"."payment_gateway"
-ADD FOREIGN KEY("gateway_id") REFERENCES "payments"("gateway_id")
+ALTER TABLE "public"."payments"
+ADD FOREIGN KEY("gateway_id") REFERENCES "payment_gateway"("gateway_id")
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-ALTER TABLE "public"."transactions"
-ADD FOREIGN KEY("payment_id") REFERENCES "payments"("payment_id")
+ALTER TABLE "public"."payments"
+ADD FOREIGN KEY("payment_id") REFERENCES "transactions"("payment_id")
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 alter table "public"."orders" enable row level security;
